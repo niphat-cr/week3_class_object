29f3fbe83101c1f8a8d1a2f4b2a202860734cfe5
niphat-cr <niphat.cr@kmitl.ac.th>
2025-07-21 11:43:18 +0700

    class objec
    class object

---
+++ week3_class_object/src/example/myapp/AquariumFish.kt
@@ -0,0 +1,39 @@
+package example.myapp
+
+interface FishAction  {
+    fun eat()
+}
+
+interface FishColor {
+    val color: String
+}
+
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+
+
+abstract class AquariumFish {
+    abstract val color: String
+}
+
+class Shark: FishAction, FishColor {
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus(fishColor: FishColor = GoldColor): FishAction by PrintingFishAction("eat algae"), FishColor by GoldColor {
+    
+}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
+
+
