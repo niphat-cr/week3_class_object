d108f95b7c54f00e69a23ef7cc8e6d7fe7c71302
niphat-cr <niphat.cr@kmitl.ac.th>
2025-08-03 11:48:11 +0700

    First Commit Codes
    Commit ไปและนะครับ

--- week3_class_object/src/Main.kt
+++ week3_class_object/src/Main.kt
@@ -1,5 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
+    for (i in 1..5){
+        println(i)
+    }
 }
